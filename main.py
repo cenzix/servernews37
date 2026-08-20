23ce5678a9e47f3b3a14bf33802e6034ec59f6d1
cenzix <yurin.max525@gmail.com>
2026-08-20 17:25:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -68,8 +68,8 @@ async def generate_post(message: types.Message):
 """
 
     try:
-        completion = client.chat.completions.create(
-            model="llama-3.3-70b-specdec",
+       completion = client.chat.completions.create(
+            model="llama-3.1-70b-versatile",  # <-- Вставьте эту
             messages=[{"role": "user", "content": prompt}],
             temperature=0.7,
             max_tokens=800
